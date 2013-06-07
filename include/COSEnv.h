a93e1db9336d574ac50c33dd511ceded1b77873e
Colin Williams <colinw7@gmail.com>
2013-06-06 21:47:35 -0400

    new files

--- include/COSEnv.h
+++ include/COSEnv.h
@@ -1,7 +1,11 @@
 #ifndef COSEnv_H
 #define COSEnv_H
 
+#include <string>
+#include <vector>
 #include <cstdlib>
+#include <cstring>
+#include <algorithm>
 
 namespace COSEnv {
   inline bool checkenv(const std::string &name) {
@@ -48,6 +52,66 @@ namespace COSEnv {
 
     for (int j = i; j < i1; j++)
       environ[j] = environ[j + 1];
+  }
+
+  inline void getNameValues(std::vector<std::string> &names, std::vector<std::string> &values) {
+    names .clear();
+    values.clear();
+
+    for (int i = 0; environ[i] != NULL; i++) {
+      std::string env_name = std::string(environ[i]);
+
+      std::string::size_type pos = env_name.find('=');
+
+      std::string env_value;
+
+      if (pos != std::string::npos) {
+        env_value = env_name.substr(pos + 1);
+        env_name  = env_name.substr(0, pos);
+      }
+
+      names .push_back(env_name );
+      values.push_back(env_value);
+    }
+  }
+
+  typedef std::pair<std::string,std::string> NameValue;
+
+  class NameValueCmp {
+   public:
+    int operator()(const NameValue &a, const NameValue &b) {
+      return (a.first < b.first);
+    }
+  };
+
+  inline void getSortedNameValues(std::vector<std::string> &names,
+                                  std::vector<std::string> &values) {
+    std::vector<NameValue> name_values;
+
+    for (int i = 0; environ[i] != NULL; i++) {
+      std::string env_name = std::string(environ[i]);
+
+      std::string::size_type pos = env_name.find('=');
+
+      std::string env_value;
+
+      if (pos != std::string::npos) {
+        env_value = env_name.substr(pos + 1);
+        env_name  = env_name.substr(0, pos);
+      }
+
+      name_values.push_back(NameValue(env_name,env_value));
+    }
+
+    std::sort(name_values.begin(), name_values.end(), NameValueCmp());
+
+    names .clear();
+    values.clear();
+
+    for (unsigned int i = 0; i < name_values.size(); i++) {
+      names .push_back(name_values[i].first );
+      values.push_back(name_values[i].second);
+    }
   }
 }
 
